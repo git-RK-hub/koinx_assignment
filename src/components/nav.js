dd34d7dff87951f968dbadb443b9ab65bfdfbc4d
rishi-scaler <43984467+git-RK-hub@users.noreply.github.com>
2022-07-10 20:49:16 +0530

    navbar added

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import Brand from '../assets/images/brand.svg';
+import Search from '../assets/images/search.svg';
+import Menu from '../assets/images/hamburger.svg';
+
 const Navbar = () => {
   return (
     <div className="dashboard-navbar">
-      Navbar  
+      <img
+        src={Brand}
+        className="dashboard-navbar__brand"
+        alt="Crypto tracker"
+      />
+      <div className="dashboard-navbar__item">
+        <img
+          src={Search}
+          className='dashboard-navbar__item-search m-5'
+          alt="Search"
+        />
+        <img
+          src={Menu}
+          className='dashboard-navbar__item-burger m-5'
+          alt="Menu"
+        />
+      </div>
     </div>
   );
 }
