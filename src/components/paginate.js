c5077ce12c8c8d2c8f87c7742dadcbe4daffc8b9
rishi-scaler <43984467+git-RK-hub@users.noreply.github.com>
2022-07-11 01:18:28 +0530

    table pagination

---
+++ src/components/paginate.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+const Paginate = ({dataLength, rowsPerPage, onPageChange, currentPage}) => {
+  const numberOfPages = dataLength / rowsPerPage;
+  const [currentActivePage, setCurrentActivePage] = useState(currentPage);
+
+  const handlePageClick = (page) => {
+    setCurrentActivePage(page);
+    onPageChange(rowsPerPage * (page-1), rowsPerPage * page);
+  }
+
+  const handlePrevClick = () => {
+    setCurrentActivePage((page) => page - 1);
+    onPageChange(rowsPerPage * (currentActivePage-2), rowsPerPage * (currentActivePage-1));
+  }
+
+  const handleNextClick = () => {
+    setCurrentActivePage((page) => page + 1);
+    onPageChange(rowsPerPage * (currentActivePage), rowsPerPage * (currentActivePage + 1));
+  }
+
+  if(numberOfPages === 1) {
+    return null;
+  } else {
+    return (
+      <div className='dashboard-paginate'>
+        <button
+          className={
+            `dashboard-paginate__button dashboard-paginate__button-nav
+              ${currentActivePage === 1 ? 'dashboard-paginate__button--disabled' : ''}`
+          }
+          onClick={handlePrevClick}
+        >
+          &lt;
+        </button>
+        {Array(numberOfPages).fill(1).map((_, idx) => (
+          <button
+            className={
+              idx+1 === currentActivePage
+              ? "dashboard-paginate__button dashboard-paginate__button--active"
+              : "dashboard-paginate__button"
+            }
+            onClick={() => handlePageClick(idx+1)}
+          >
+            {idx + 1}
+          </button>
+        ))}
+        <button
+          className={
+            `dashboard-paginate__button dashboard-paginate__button-nav
+              ${currentActivePage === numberOfPages ? 'dashboard-paginate__button--disabled' : ''}`
+          }
+          onClick={handleNextClick}
+        >
+          &gt;
+        </button>
+      </div>
+    )
+  }
+}
+ 
+export default Paginate;
--- src/components/table.js
+++ src/components/table.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState } from 'react';
 
 import { useAPI } from '../context/hook';
+import Paginate from './paginate';
 
 const Table = ({desktopColumns, mobileColumns}) => {
   const { data } = useAPI();
+  const [rowCount, setRowCount] = useState(10);
+  const [rowsData, setRowsData] = useState([]);
+  
+  console.log(rowsData);
+  const getRowsData = useCallback((startIdx, endIdx) => {
+    setRowsData(data.slice(startIdx, endIdx));
+  }, [data]);
+  
+  useEffect(() => {
+    getRowsData(0, rowCount);
+  },[data, getRowsData, rowCount]);
+  
+  const changeRowCount = (e) => {
+    setRowCount(Number(e.target.value));
+  }
 
   return (
     <div className='dashboard-table'>
+      <div className='dashboard-table__row-count row align-c justify-end'>
+        <span className="m-5 dead-text small-font-size">show rows</span>
+        <select value={rowCount} onChange={changeRowCount}>
+          <option value={10}>10</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+        </select>
+      </div>
       <div className='dashboard-table__header'>
         <div className='dashboard-table__row'>
-          {desktopColumns.map((column, idx) => {
-            return (
-              <div className='dashboard-table__column' key={idx}>
-                <div className="row align-c justify-end">
-                  {column.name}
-                  {column.headerIcon && <img className="m-5" src={column.headerIcon} alt="down-arrow" />}
-                </div>
+          {desktopColumns.map((column, idx) => (
+            <div className='dashboard-table__column' key={idx}>
+              <div className="row align-c justify-end">
+                {column.name}
+                {column.headerIcon && <img className="m-5" src={column.headerIcon} alt="down-arrow" />}
               </div>
-            )
-          })}
+            </div>
+          ))}
         </div>
       </div>
       <div className='dashboard-table__body'>
-        {data.map((row, idx) => {
-          return (
-            <div key={row.id} className="dashboard-table__row">
-              {desktopColumns.map((column) => {      
-                return (
-                  <div key={Math.random()} className='dashboard-table__column'>{column.value(row, idx)}</div>
-                )
-              })}
-            </div>
-          )
-        })}
+        {rowsData.map((row, idx) => (
+          <div key={row.id} className="dashboard-table__row">
+            {desktopColumns.map((column) => (
+                <div key={Math.random()} className='dashboard-table__column'>{column.value(row, idx)}</div>
+              )
+            )}
+          </div>
+        ))}
       </div>
+      <Paginate
+        currentPage={1}
+        dataLength={data.length}
+        rowsPerPage={rowCount}
+        onPageChange={getRowsData}
+      />
     </div>
   );
 }
