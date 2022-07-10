d6fb1526f10596ac67fca36ed877a5cadac942c5
rishi-scaler <43984467+git-RK-hub@users.noreply.github.com>
2022-07-11 02:47:47 +0530

    tags only on desktop screen

--- src/ui/tags.js
+++ src/ui/tags.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 import Tag from '../components/tag';
 import Star from '../assets/images/star.png';
+import useWindowSize from '../utils/useWindowSize';
 
 const TagsUi = () => {
-  return (
-    <div className='dashboard-tags'>
-      <Tag name="Favourites" icon={Star} />
-      <Tag name="CryptoCurrencies" />
-      <Tag name="DeFi" />
-      <Tag name="NFTs & Collectibles" />
-    </div>
-  );
+  const { isMobile, isTablet } = useWindowSize();
+
+  if (isMobile || isTablet) {
+    return null;
+  } else {
+    return (
+      <div className='dashboard-tags'>
+        <Tag name="Favourites" icon={Star} />
+        <Tag name="CryptoCurrencies" />
+        <Tag name="DeFi" />
+        <Tag name="NFTs & Collectibles" />
+      </div>
+    );
+  }
 }
 
 export default TagsUi;
