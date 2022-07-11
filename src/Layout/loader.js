b065ed0c4a17e6567e9166c1d9c9c80bd206fefd
rishi-scaler <43984467+git-RK-hub@users.noreply.github.com>
2022-07-11 16:51:09 +0530

    nit

--- src/Layout/loader.js
+++ src/Layout/loader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LoadingLayout = () => {
-  return <div class="dashboard-loader"></div>
+  return <div className="dashboard-loader"></div>
 }
  
 export default LoadingLayout;
